fec8aaa1dbd7ba228b4b8a8956bf5ecb02a7b83b
Nathan Bridgewater <nathan@iws.io>
2020-02-10 16:39:27 -0600

    Adding docker-compose for testing, and an example mock module test.

--- tests/people-list-page.test.js
+++ tests/people-list-page.test.js
@@ -4,22 +4,23 @@ import { act } from "react-dom/test-utils"
 import PeoplePage from "../src/client/components/start-context/list-page"
 import {Router, Switch, Route} from "react-router-dom"
 import {createBrowserHistory} from "history"
-import AppProvider from "../src/client/components/start-context/app-provider"
+
+import MockedDefaultLayout from "../src/client/components/start-context/default-layout"
+jest.mock("../src/client/components/start-context/default-layout", () => {return ({children}) => (<div>{children}</div>)})
 
 let customHistory = null
 let Wrapper = null
-
 let container = null
+
 beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement("div")
   document.body.appendChild(container)
   customHistory = createBrowserHistory()
   Wrapper = ({children}) => (
-    <AppProvider><Router history={customHistory}><Switch><Route>{children}</Route></Switch></Router></AppProvider>
+    <Router history={customHistory}><Switch><Route>{children}</Route></Switch></Router>
   )
 })
-
 afterEach(() => {
   // cleanup on exiting
   unmountComponentAtNode(container)
@@ -27,21 +28,25 @@ afterEach(() => {
   container = null
 })
 
-it("renders the people page with data", async () => {
-  jest.mock("../src/client/lib/fetch-json", (url, options) => {
-    const fakePeople = [{
-      name: "Joni Baez",
-      age: 32,
-      id: "1"
-    }]
-    Promise.resolve({
-      json: () => Promise.resolve(fakePeople)
-    })
-  })
-  await act(async () => {
-    render(<Wrapper><PeoplePage /></Wrapper>, container)
-  })
-  expect(container.textContent).toContain("No people to display")
-
+afterAll(() => {
   jest.resetModules()
+})
+
+describe("People Page - We've mocked DefaultLayout to remove it from the rendered markup.", () => {
+  it("renders the people page with data", async () => {
+    jest.mock("../src/client/lib/fetch-json", (url, options) => {
+      const fakePeople = [{
+        name: "Joni Baez",
+        age: 32,
+        id: "1"
+      }]
+      Promise.resolve({
+        json: () => Promise.resolve(fakePeople)
+      })
+    })
+    await act(async () => {
+      render(<Wrapper><PeoplePage /></Wrapper>, container)
+    })
+    expect(container.textContent).toContain("No people to display")
+  })
 })
