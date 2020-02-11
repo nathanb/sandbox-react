2798ebd932751d08d3e1f9823587a6d5417c64c2
Nathan Bridgewater <nathan@iws.io>
2020-02-11 13:21:58 -0600

    Added columns, but forgot to update the test; hey it worked\!

--- tests/people-list.test.js
+++ tests/people-list.test.js
@@ -38,7 +38,7 @@ it("renders table with rows of people", () => {
     render(<Wrapper><PeopleList data={[{id: "1", name: "test", age: 44}]} /></Wrapper>, container)
   })
   expect(container.querySelectorAll("tbody tr")).toHaveLength(1)
-  expect(container.querySelectorAll("tbody tr td")).toHaveLength(2)
+  expect(container.querySelectorAll("tbody tr td")).toHaveLength(5)
 
   act(() => {
     render(<Wrapper><PeopleList data={[{id: "1", name: "test", age: 44}, {id: "2", name: "test 2", age: 55}]} /></Wrapper>, container)
