e281135c946c198fa54dc206274cda4cb346b372
Nathan Bridgewater <nathan@iws.io>
2020-02-14 15:35:19 -0600

    reset

--- src/client/components/common/alert.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Alert = ({message, type}) => (
-  message ? (<div className={`alert alert-${type}`}>{message}</div>) : null
-)
-Alert.defaultProps = {
-  type: "danger"
-}
-export default Alert
