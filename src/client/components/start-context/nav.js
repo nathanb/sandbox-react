2e9790658c1b21e1d323840f6cb3ef3272976ff2
Nathan Bridgewater <nathan@iws.io>
2020-02-15 10:33:43 -0600

    Enabling basic responsive hamburger menu; no bootstrap.js

--- src/client/components/start-context/nav.js
+++ src/client/components/start-context/nav.js
@@ -1,35 +1,38 @@
-import React from "react"
+import React,{useState} from "react"
 import {Link, NavLink} from "react-router-dom"
 import CurrentUserNavItem from "./current-user-nav"
 import RandomDataUpdateItem from "./random-data-nav"
 
-const HeaderNav = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">React Sandbox</Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+const HeaderNav = () => {
+  // We're not being fancy here. We could use key-frames to animate the markup or make it pretty but for simplicity; we'll just hide/show it
+  const [show, setShow] = useState(false)
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">React Sandbox</Link>
+      <button className="navbar-toggler" type="button" onClick={() => {setShow(!show)}} aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-    <div className="collapse navbar-collapse" id="navbarColor03">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <NavLink to="/" exact={true} className="nav-link">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/people" exact={true} className="nav-link">People</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/chart" exact={true} className="nav-link">Test Chart (code splitting)</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/about" exact={true} className="nav-link">About</NavLink>
-        </li>
-      </ul>
-      <RandomDataUpdateItem />
-      <CurrentUserNavItem />
-      <span className="nav-text">v1.0.0</span>
-    </div>
-  </nav>
-)
-
+      <div className={`collapse navbar-collapse ${show ? "show" : ""}`}>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink to="/" exact={true} className="nav-link">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/people" exact={true} className="nav-link">People</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/chart" exact={true} className="nav-link">Test Chart (code splitting)</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" exact={true} className="nav-link">About</NavLink>
+          </li>
+        </ul>
+        <RandomDataUpdateItem />
+        <CurrentUserNavItem />
+        <span className="nav-text">v1.0.0</span>
+      </div>
+    </nav>
+  )
+}
 export default HeaderNav
